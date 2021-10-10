316050a16aa028ca4e7143b864581d381387ef6b
Faridsasaki <faridrishardi18@gmail.com>
2021-10-11 00:09:57 +0700

    menambahkan pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya : Farid Rishardi
+    <br> Saya tinggal di daerah : Jln Cipinang Jagal <br>
+    Olahraga yang saya : Futsal Ball ";
+  }
+}
